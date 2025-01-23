1d0f7735127b05a4671096cae9bade21cdea31c6
Elivince <elivincemateos.timbas@bicol-u.edu.ph>
2025-01-23 14:02:56 +0800

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,97 @@
+<script lang="ts">
+    import { onMount } from "svelte";
+
+    let rates: Record<string, number> = {};
+    let fromCurrency = "PHP";
+    let toCurrency = "USD";
+    let amount = 50;
+    let total = "";
+
+    onMount(async () => {
+        try {
+            const response = await fetch(
+                `https://api.freecurrencyapi.com/v1/latest?apikey=${import.meta.env.VITE_API_KEY}`
+            );
+            const data = await response.json();
+            rates = data.data;
+        } catch (error) {
+            console.error("Error fetching currency data:", error);
+        }
+    });
+
+    $: if (rates[fromCurrency] && rates[toCurrency]) {
+        const fromRate = rates[fromCurrency];
+        const toRate = rates[toCurrency];
+        total = ((amount * toRate) / fromRate).toFixed(2);
+    }
+</script>
+
+<style>
+    h1 {
+        text-align: center;
+    }
+    .converter{
+        display: flex;
+        flex-direction: column;
+        background-color: #f4f4f9;
+        border-radius: 8px;
+        box-shadow: 0 4px 10px rgba(0, 0, 0, 0.1);
+        width: 100%;
+        max-width: 400px;
+        padding: 20px;
+    }
+
+    .inputCenter{
+        display: flex;
+        justify-content: center;
+    }
+
+    input[type="number"]{
+        width: 30%;
+        justify-content: center;
+    }
+
+    .valueSelector{
+        display: flex;
+        justify-content: center;
+        width: 100%;
+    }
+
+    h2{
+        font-size: larger;
+        text-align: center;
+    }
+
+   
+</style>
+
+<div class="converter">
+    <h1>Currency Converter</h1>
+
+    <div class="inputCenter">
+        <label style="padding-right: 10px;" for="inputNumber">Amount: </label><br />
+        
+        <input id="inputNumber" type="number" bind:value={amount} placeholder="Amount" />
+    </div>
+
+    <br />
+
+    <div class="valueSelector">
+        <label style="padding-right: 10px;" for="from">From: </label>
+        <select id="from" bind:value={fromCurrency}>
+            {#each Object.keys(rates) as currency}
+                <option value={currency}>{currency}</option>
+            {/each}
+        </select>
+        
+        <label style="padding-right: 10px; padding-left: 50px" for="to">To: </label>
+        <select id="to" bind:value={toCurrency}>
+            {#each Object.keys(rates) as currency}
+                <option value={currency}>{currency}</option>
+            {/each}
+        </select>
+    </div>
+    <br />
+
+    <h2>{amount} {fromCurrency} = {total || "..."} {toCurrency}</h2>
+</div>
